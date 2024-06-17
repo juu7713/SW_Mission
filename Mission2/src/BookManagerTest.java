165c6854080e460c7d4f610d3fd1aed55766d4a4
keuns0407 <kimeunsu0407@gmail.com>
2024-06-17 20:53:09 +0900

    update Test

--- Mission2/src/BookManagerTest.java
+++ Mission2/src/BookManagerTest.java
@@ -6,18 +6,21 @@ import org.junit.jupiter.api.Test;
 class BookManagerTest {
 	public BookManager bookManager; 
 	
+	//Test를 위한 bookManager생성
 	@BeforeEach
 	void setUp() {
 		bookManager = new BookManager();
 	}
 	
+	//bookManager내의 addBook() Test
 	@Test
 	void testAddBook() {
 		bookManager.addBook(1, "자바 기초", "Jane", 2021);
-		bookManager.addBook(1, "자바 기초", "Jane", 2021);
-		bookManager.searchBook(1);
+		bookManager.addBook(1, "자바 기초", "Jane", 2021); //같은Book 생성 후 오류 메시지 확인
+		bookManager.searchBook(1); //add 확인
 	}
-
+	
+	//bookManager내의 searchBook() Test
 	@Test
 	void testSearchBook() {
 		bookManager.addBook(1, "자바 기초", "Jane", 2021);
@@ -26,16 +29,17 @@ class BookManagerTest {
 		bookManager.searchBook(1);
 		bookManager.searchBook(2);
 		bookManager.searchBook(3);
-		bookManager.searchBook(4);
+		bookManager.searchBook(4); //오류 메시지 확인
 	}
 
+	//bookManager내의 deleteBook() Test
 	@Test
 	void testDeleteBook() {
 		bookManager.addBook(1, "자바 기초", "Jane", 2021);
 		bookManager.addBook(2, "소프트웨어 공학", "Tom", 2014);
 		bookManager.deleteBook(1);
-		bookManager.deleteBook(1);
-		bookManager.searchBook(1);
+		bookManager.deleteBook(1); //오류 메시지 확인
+		bookManager.searchBook(1); //오류 메시지 확인
 		
 	}
 
